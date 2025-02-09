d5449ef494c2f8bec4ce9b05e8fc1e0a0dd07c53
jaredborders <44350516+JaredBorders@users.noreply.github.com>
2025-02-08 19:40:02 -0500

    🧹 clean up and fmt

--- benches/async_benchmark.rs
+++ benches/async_benchmark.rs
@@ -6,47 +6,59 @@ use create3::{
 };
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
+/// bench single-threaded generate salt.
 fn generate_salt_benchmark(c: &mut Criterion) {
     let prefix = black_box("0000");
-    let deployer = black_box(hex::decode("0fC5025C764cE34df352757e82f7B5c4Df39A836").unwrap());
-
+    let deployer = black_box(
+        hex::decode("0fC5025C764cE34df352757e82f7B5c4Df39A836").unwrap(),
+    );
     c.bench_function("single-threaded generate salt", |b| {
         b.iter(|| generate_salt(&deployer, prefix))
     });
 }
 
+/// bench multi-threaded generate salt.
 fn generate_salt_multithread_benchmark(c: &mut Criterion) {
     let prefix = black_box("0000");
-    let deployer = black_box(hex::decode("0fC5025C764cE34df352757e82f7B5c4Df39A836").unwrap());
-
+    let deployer = black_box(
+        hex::decode("0fC5025C764cE34df352757e82f7B5c4Df39A836").unwrap(),
+    );
     c.bench_function("multi-threaded generate salt", |b| {
         b.iter(|| generate_salt_multithread(&deployer, prefix, 6))
     });
 }
 
+/// bench single-threaded generate salt with salt prefix.
 fn generate_salt_prefix_benchmark(c: &mut Criterion) {
     let prefix = black_box("0000");
     let salt_prefix = black_box("my_prefix_");
-    let deployer = black_box(hex::decode("0fC5025C764cE34df352757e82f7B5c4Df39A836").unwrap());
-
-    c.bench_function("single-threaded generate salt", |b| {
+    let deployer = black_box(
+        hex::decode("0fC5025C764cE34df352757e82f7B5c4Df39A836").unwrap(),
+    );
+    c.bench_function("single-threaded generate salt prefix", |b| {
         b.iter(|| generate_salt_prefix(&deployer, salt_prefix, prefix))
     });
 }
 
+/// bench multi-threaded generate salt with salt prefix.
 fn generate_salt_prefix_multithread_benchmark(c: &mut Criterion) {
     let prefix = black_box("0000");
     let salt_prefix = black_box("my_prefix_");
-    let deployer = black_box(hex::decode("0fC5025C764cE34df352757e82f7B5c4Df39A836").unwrap());
-
-    c.bench_function("multi-threaded generate salt", |b| {
-        b.iter(|| generate_salt_prefix_multithread(&deployer, salt_prefix, prefix, 6))
+    let deployer = black_box(
+        hex::decode("0fC5025C764cE34df352757e82f7B5c4Df39A836").unwrap(),
+    );
+    c.bench_function("multi-threaded generate salt prefix", |b| {
+        b.iter(|| {
+            generate_salt_prefix_multithread(&deployer, salt_prefix, prefix, 6)
+        })
     });
 }
 
 criterion_group!(
     name = generate_salt_benches;
-    config = Criterion::default().measurement_time(Duration::from_secs(15));
-    targets = generate_salt_benchmark, generate_salt_multithread_benchmark, generate_salt_prefix_benchmark, generate_salt_prefix_multithread_benchmark
+    config = Criterion::default()
+        .measurement_time(Duration::from_secs(15));
+    targets = generate_salt_benchmark, generate_salt_multithread_benchmark,
+        generate_salt_prefix_benchmark, generate_salt_prefix_multithread_benchmark
 );
 criterion_main!(generate_salt_benches);
