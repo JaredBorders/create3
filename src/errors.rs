8365057c786ef6c35e80dfab4f4af0745f75464d
jaredborders <44350516+JaredBorders@users.noreply.github.com>
2025-02-08 19:39:58 -0500

    🧹 clean up and fmt

--- src/errors.rs
+++ src/errors.rs
@@ -1,11 +1,11 @@
 use std::{error::Error, fmt::Display};
 
-/// Errors for generating a CREATE3 salt.
+/// errors for generating a create3 salt.
 #[derive(Debug, PartialEq)]
 pub enum Create3GenerateSaltError {
-    /// Occurs if the prefix is too long. The prefix must be less than or equal to 20 bytes.
+    /// prefix is too long (max 20 bytes).
     PrefixTooLong,
-    // Occurs if the prefix is not a hex encoded string. The prefix must be in hexadecimal format.
+    /// prefix is not hex encoded.
     PrefixNotHexEncoded,
 }
 
@@ -18,10 +18,12 @@ impl Error for Create3GenerateSaltError {
 impl Display for Create3GenerateSaltError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str(match self {
-            Create3GenerateSaltError::PrefixTooLong => 
-                "Create3GenerateSaltError::PrefixTooLong: the prefix is too long. The prefix must be less than or equal to 20 bytes.",
-            Create3GenerateSaltError::PrefixNotHexEncoded => 
-                "Create3GenerateSaltError::PrefixNotHexEncoded: the prefix is not a hex encoded string. The prefix must be in hexadecimal format.",
+            Create3GenerateSaltError::PrefixTooLong => {
+                "prefix too long (max 20 bytes)."
+            }
+            Create3GenerateSaltError::PrefixNotHexEncoded => {
+                "prefix not hex encoded."
+            }
         })
     }
 }
